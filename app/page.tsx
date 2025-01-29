6ec967d619e86be45754f054bb0adcfc764d0074
dmelotik <dylanmelotik@gmail.com>
2025-01-29 01:08:59 -0700

    remove unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from 'next/link'
 import ArrowIcon from '@/components/ArrowIcon'
 import FlipCard from '@/components/FlipCard'
 export default function Home() {
